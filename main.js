cd2581cae48fc0abced6f1b92c3573229ff271ad
jinioh88 <jinioh88@gmail.com>
2021-01-23 11:49:56 +0900

    totalAmount 빼기

--- main.js
+++ main.js
@@ -70,8 +70,16 @@ function usd(aNumber) {
     }).format(aNumber/100);
 }
 
-function statement(invoice, plays) {
+function appleSauce() {
     let totalAmount = 0;
+    for (let perf of invoice.performances) {
+        totalAmount += amountFor(perf);
+    }    
+
+    return totalAmount;
+}
+
+function statement(invoice, plays) {
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     const format = new Intl.NumberFormat("en-US", {
         style: "currency",
@@ -82,8 +90,8 @@ function statement(invoice, plays) {
     for (let perf of invoice.performances) {
         //  청구 내역 출력
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }    
+    let totalAmount = appleSauce();
 
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
