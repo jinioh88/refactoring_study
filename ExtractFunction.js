ec4039994beff6424e08580f107aa207ddfe2a26
jinioh88 <jinioh88@gmail.com>
2021-01-26 23:32:39 +0900

    함수 추출하기 적용

--- ExtractFunction.js
+++ ExtractFunction.js
@@ -1,31 +1,37 @@
+
 function printOwing(invoice) {
     printBanner();
-    let outstanding = calculateOutstanding();
 
-    function printDetails(outstanding) {
-        console.log(`고객명: ${invoice.customer}`);
-        console.log(`채무액: ${outstanding}`);
-    }
+    const outstanding = calculateOutstanding(invoice);
+
+    recordDueDate(invoice);
+
+    printDetail(invoice, outstanding);
 }
 
-function printOwing(invoice) {
-    let outstanding = 0;
+function calculateOutstanding(invoice) {
+    let result = 0;
+    for (const o of invoice.orders) {
+        result += o.amount;
+    }
 
+    return result;
+}
+
+function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+function printBanner() {
     console.log('****************');
     console.log('*** 고객 채무 **');
     console.log('****************');
+}
 
-    // 미해결 채무(outstanding)를 계산
-    for (const o of invoice.orders) {
-        outstanding += o.amount;
-    }
-
-    // 마감일 (dueDate)을 기록
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-
-    // 세부 사항을 출력
+function printDetail(invoice, outstanding) {
     console.log(`고객명: ${invoice.customer}`);
     console.log(`채무액: ${outstanding}`);
     console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
+
