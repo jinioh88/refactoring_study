d33a8e96d7211720ec9ec4be7055fa026d026c88
jinioh88 <jinioh88@gmail.com>
2021-01-26 22:52:08 +0900

    함수 추출하기 before

--- ExtractFunction.js
+++ ExtractFunction.js
@@ -7,3 +7,25 @@ function printOwing(invoice) {
         console.log(`채무액: ${outstanding}`);
     }
 }
+
+function printOwing(invoice) {
+    let outstanding = 0;
+
+    console.log('****************');
+    console.log('*** 고객 채무 **');
+    console.log('****************');
+
+    // 미해결 채무(outstanding)를 계산
+    for (const o of invoice.orders) {
+        outstanding += o.amount;
+    }
+
+    // 마감일 (dueDate)을 기록
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+
+    // 세부 사항을 출력
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
