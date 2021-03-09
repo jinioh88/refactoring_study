f2f79d4c5aca43710f595443e77aab1b625f7a73
jinioh88 <jinioh88@gmail.com>
2021-03-10 08:09:11 +0900

    필드 옮기기

---
+++ MoveField.js
@@ -0,0 +1,26 @@
+class Customer {
+    constructor(name, discountRate) {
+        this._name = name;
+        this._contract = new CustomerContract(dateToday());
+        this._setDiscountRate(discountRate);
+    }
+
+    get discountRate() {return this._contract._discountRate;}
+    _setDiscountRate(aNumber) {this._contract._discountRate = aNumber;}
+    becomePreferred() {
+        this._setDiscountRate(this._discountRate + 0.03);
+    }
+    applyDiscount(amount) {
+        return amount.subtract(amount.multiply(this._discountRate));
+    }
+}
+
+class CustomerContract {
+    constructor(startDate, discountRate) {
+        this._startDate = startDate;
+        this._discountRate = discountRate;
+    }
+
+    get discountRate() {return this._discountRate;}
+    set discountRate(arg) {this._discountRate = arg;}
+}
